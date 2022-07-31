36b3ceb11e613cca0103968969aa11286462094e
benluks <70067243+benluks@users.noreply.github.com>
2022-07-31 18:34:34 +0200

    anomaly detection in autograd

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ config = yaml.load(open(args.config, 'r'), Loader=yaml.FullLoader)
 
 
 if __name__ == '__main__':
-
+    torch.autograd.set_detect_anomaly(True)
     trainer = Trainer(args, **config['hparams'])
     trainer.build(**config)
     
